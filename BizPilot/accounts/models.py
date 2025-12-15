5df10fc5c495db7e6392b6f26304f95e613fc9d2
Ibrahim Olawoyin <woyin365@gmail.com>
2025-12-15 17:56:18 +0000

    BizPilot: feat(accounts&categories): implement custom user authentication with AbstractUser, country field, category linkage, and JWT login

--- BizPilot/accounts/models.py
+++ BizPilot/accounts/models.py
@@ -14,15 +14,15 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     #username = models.CharField(max_length=150, blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
-    country = CountryField(max_length=50, blank=True, null=True)
+    country = CountryField(blank=True, null=True)
     company = models.CharField(max_length=100, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
-""" email is set to be required for login which overrides the Django default username 
-Also, stated built in fields would be populated for signup """
+# Use email as the login identifier
+# Also, stated built in fields would be populated for signup
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-def __str__(self):
+    def __str__(self):
         return self.username
